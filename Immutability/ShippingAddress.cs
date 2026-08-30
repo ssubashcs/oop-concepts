bc21995df1e5756d4f8e95ac69318aac72e413ea
ssubashcs <244132626+ssubashcs@users.noreply.github.com>
2026-08-30 13:47:11 +0530

    feat: immutability and value objects

---
+++ Immutability/ShippingAddress.cs
@@ -0,0 +1,23 @@
+﻿namespace ObjectOrientedProgramming.Immutability
+{
+    public sealed record ShippingAddress
+    {   
+        public string Street { get; }
+        public string City { get; }
+        public string PostalCode { get; }
+        public string Country { get; }
+
+        public ShippingAddress(string street, string city, string postalCode, string country)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(street);
+            ArgumentException.ThrowIfNullOrWhiteSpace(city);
+            ArgumentException.ThrowIfNullOrWhiteSpace(postalCode);
+            ArgumentException.ThrowIfNullOrWhiteSpace(country);
+
+            Street = street;
+            City = city;
+            PostalCode = postalCode;
+            Country = country;
+        }
+    }
+}
